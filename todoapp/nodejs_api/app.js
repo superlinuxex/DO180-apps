73229bf38f1d3203d98a289fc4136dadd852e359
Superlinuxec <superlinuxec@gmail.com>
2025-03-16 12:58:46 +0100

    Update app.js
    añadimos el middware

--- todoapp/nodejs_api/app.js
+++ todoapp/nodejs_api/app.js
@@ -1,37 +1,44 @@
 const restify = require('restify');
 const corsMiddleware = require('restify-cors-middleware');
+
 const controller = require('./controllers/items');
 const serverinfo = require('./controllers/serverinfo');
+
 const db = require('./models/db');
 const model = require('./models/items');
 
 const cors = corsMiddleware({
-  origins: ['*'],
-  allowHeaders: ['Authorization'],
-  exposeHeaders: ['Authorization']
+    origins: ['*'],
+    allowHeaders: ['Authorization'],
+    exposeHeaders: ['Authorization']
 });
 
-model.connect(db.params, (err) => {
-  if (err) throw err;
+model.connect(db.params, function(err) {
+    if (err) throw err;
 });
 
 const server = restify.createServer();
 server.pre(cors.preflight);
 server.use(cors.actual);
-server.use(restify.plugins.queryParser());
-server.use(restify.plugins.bodyParser());
+
+server.use(restify.fullResponse());
+server.use(restify.queryParser());
+server.use(restify.bodyParser());
 
 controller.context(server, '/todo/api', model);
 serverinfo.context(server, '/todo/api');
 
 const port = process.env.PORT || 30080;
-server.listen(port, () => {
-  console.log(`App is ready at: ${port}`);
+server.listen(port, function(err) {
+    if (err) {
+        console.error(err);
+    } else {
+        console.log('App is ready at : ' + port);
+    }
 });
 
 if (process.env.environment === 'production') {
-  process.on('uncaughtException', (err) => {
-    console.error(JSON.parse(JSON.stringify(err, ['stack', 'message', 'inner'], 2)));
-  });
+    process.on('uncaughtException', function(err) {
+        console.error(JSON.parse(JSON.stringify(err, ['stack', 'message', 'inner'], 2)));
+    });
 }
-
