c118dc9148956854d432ee109f6027728e887e40
Superlinuxec <superlinuxec@gmail.com>
2025-03-16 11:14:22 +0100

    Update db.js
    actualizacion version

--- todoapp/nodejs_api/models/db.js
+++ todoapp/nodejs_api/models/db.js
@@ -1,12 +1,16 @@
 
+const { Sequelize } = require('sequelize');
+
 module.exports.params = {
-    dbname: process.env.MYSQL_ENV_MYSQL_DATABASE,
-    username: process.env.MYSQL_ENV_MYSQL_USER,
-    password: process.env.MYSQL_ENV_MYSQL_PASSWORD,
-    params: {
-        host: "mysql",
-        port: "3306",
-        dialect: 'mysql'
-    }
+  dbname: process.env.MYSQL_ENV_MYSQL_DATABASE,
+  username: process.env.MYSQL_ENV_MYSQL_USER,
+  password: process.env.MYSQL_ENV_MYSQL_PASSWORD,
+  params: {
+    host: process.env.MYSQL_HOST || 'mysql',
+    port: process.env.MYSQL_PORT || '3306',
+    dialect: 'mysql',
+    dialectModule: require('mysql2'),
+    logging: false
+  }
 };
 
